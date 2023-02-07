78d11b3e8f3ce584e99c4faf33a6c2d6a0ad1731
Firessean <themrsaus@gmail.com>
2023-02-07 18:27:49 +0200

    Learned how to make pause

--- Udemy-WDIO/test/specs/example.e2e.js
+++ Udemy-WDIO/test/specs/example.e2e.js
@@ -2,6 +2,11 @@
 describe('My first test suite', () => {
   it('my first wdio test', async () => {
     const exampleURL = 'https://example.com';
+    let smallPause = 2000;
+    let bigPause = 5000;
     await browser.url(exampleURL);
+    await browser.pause(smallPause);
+    await browser.url('https://www.saucedemo.com/');
+    await browser.pause(bigPause);
   });
 });
