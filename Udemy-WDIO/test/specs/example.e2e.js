57626493f16b1ee0b8809cf13f859a2cff6aded4
Firessean <themrsaus@gmail.com>
2023-02-07 18:16:06 +0200

    Learned how to load website

--- Udemy-WDIO/test/specs/example.e2e.js
+++ Udemy-WDIO/test/specs/example.e2e.js
@@ -1,13 +1,7 @@
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await browser.url(`https://the-internet.herokuapp.com/login`)
-
-        await $('#username').setValue('tomsmith')
-        await $('#password').setValue('SuperSecretPassword!')
-        await $('button[type="submit"]').click()
-
-        await expect($('#flash')).toBeExisting()
-        await expect($('#flash')).toHaveTextContaining(
-            'You logged into a secure area!')
-    })
-})
+'use strict';
+describe('My first test suite', () => {
+  it('my first wdio test', async () => {
+    const exampleURL = 'https://example.com';
+    await browser.url(exampleURL);
+  });
+});
