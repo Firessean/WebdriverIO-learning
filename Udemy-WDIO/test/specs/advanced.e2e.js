6f284b40d4d8858ee40fbf97a753f4afa99552f2
Firessean <themrsaus@gmail.com>
2023-02-09 15:13:41 +0200

    Learned about before and beforeEach functions

--- Udemy-WDIO/test/specs/advanced.e2e.js
+++ Udemy-WDIO/test/specs/advanced.e2e.js
@@ -1,8 +1,25 @@
 'use strict';
 describe('Advanced Testing', () => {
-  it('File Upload', async () => {
+  beforeEach(async () => {
     await browser.url('https://the-internet.herokuapp.com/upload');
+  });
+  it('File Upload 1', async () => {
+    const filePath = './my-screenshot.png';
+    const remoteFilePath = await browser.uploadFile(filePath);
+    await $('#file-upload').setValue(remoteFilePath);
+    await $('#file-submit').click();
+    await browser.pause(5000);
+  });
 
+  it('File Upload 2', async () => {
+    const filePath = './my-screenshot.png';
+    const remoteFilePath = await browser.uploadFile(filePath);
+    await $('#file-upload').setValue(remoteFilePath);
+    await $('#file-submit').click();
+    await browser.pause(5000);
+  });
+
+  it('File Upload 3', async () => {
     const filePath = './my-screenshot.png';
     const remoteFilePath = await browser.uploadFile(filePath);
     await $('#file-upload').setValue(remoteFilePath);
