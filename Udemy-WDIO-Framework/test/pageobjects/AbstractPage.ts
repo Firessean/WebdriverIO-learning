7c9725426ec1b6ff9bc31f775704752351e367eb
Firessean <themrsaus@gmail.com>
2023-02-13 16:25:11 +0200

    Added data helpers

--- Udemy-WDIO-Framework/test/pageobjects/AbstractPage.ts
+++ Udemy-WDIO-Framework/test/pageobjects/AbstractPage.ts
@@ -1,3 +1,5 @@
+import * as crypto from 'crypto';
+
 export default class AbstractPage {
   public async waitForSeconds(seconds: number) {
     await browser.pause(seconds * 1000);
@@ -13,5 +15,13 @@ export default class AbstractPage {
 
   public async takeScreenshot(path: string) {
     await browser.saveScreenshot(path);
+  }
+
+  public async generateRandomNumber() {
+    return Math.floor(Math.random() * 100000 + 1);
+  }
+
+  public async generateRandomString() {
+    return crypto.randomBytes(20).toString('hex');
   }
 }
