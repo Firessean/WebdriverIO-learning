a6ee3b652396ecade0b15b4622034c4b69bbf71c
Firessean <themrsaus@gmail.com>
2023-02-21 14:05:26 +0200

    Add search.feature

--- Udemy-WDIO-Cucumber/features/step-definitions/steps.js
+++ Udemy-WDIO-Cucumber/features/step-definitions/steps.js
@@ -1,9 +1,13 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
 
 import LoginPage from '../pageobjects/login.page.js';
+import HomePage from '../pageobjects/home.page.js';
 
 Given(/^I am on the login page$/, async () => {
   await LoginPage.visit();
+});
+Given(/^I am on the home page$/, async () => {
+  await HomePage.visit();
 });
 
 When(/^I login with (\w+) and (.+)$/, async (username, password) => {
@@ -14,6 +18,15 @@ When(/^I login with invalid credentials$/, async () => {
   await LoginPage.login('invalid-username', 'invalid-password');
 });
 
+When(/^I search for "([^"]*)"$/, async (text) => {
+  await HomePage.searchFor(text);
+});
+
 Then(/^I should see error message$/, async () => {
   await LoginPage.assertErrormessage();
+});
+
+Then(/^I should see results$/, async () => {
+  const selector = await $('h2');
+  await expect(selector).toBeDisplayed();
 });
