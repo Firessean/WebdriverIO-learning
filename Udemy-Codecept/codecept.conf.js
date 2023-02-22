766040863a2e811477285927d45d4a9904d75ed4
Firessean <themrsaus@gmail.com>
2023-02-22 16:56:18 +0200

    Add screenshots on fail

--- Udemy-Codecept/codecept.conf.js
+++ Udemy-Codecept/codecept.conf.js
@@ -30,7 +30,7 @@ exports.config = {
       enabled: true,
     },
     screenshotOnFail: {
-      enabled: false,
+      enabled: true,
     },
   },
 };
--- Udemy-Codecept/example_test.js
+++ Udemy-Codecept/example_test.js
@@ -3,4 +3,5 @@ Feature('Example Test Suite');
 Scenario('My first test', ({ I }) => {
   I.amOnPage('https://example.com/');
   I.seeElement('h1');
+  I.seeElement('.error');
 });
